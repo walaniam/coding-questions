59c4cdbfc141f89fffe43f9ca97028c36dfbc785
walaniam <mariusz.walania@gmail.com>
2025-11-12 12:50:38 +0100

    SumOfRootToLeafNumbersTest - more test cases

--- src/test/java/walaniam/coding/leetcode/SumOfRootToLeafNumbersTest.java
+++ src/test/java/walaniam/coding/leetcode/SumOfRootToLeafNumbersTest.java
@@ -3,7 +3,11 @@ package walaniam.coding.leetcode;
 import org.junit.jupiter.api.Test;
 import walaniam.coding.TreeNode;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SumOfRootToLeafNumbersTest {
+
+    private final SumOfRootToLeafNumbers underTest = new SumOfRootToLeafNumbers();
 
     @Test
     void testSum() {
@@ -15,13 +19,45 @@ class SumOfRootToLeafNumbersTest {
         n9.addLeft(5);
         n9.addRight(1);
 
-
-        SumOfRootToLeafNumbers underTest = new SumOfRootToLeafNumbers();
         int sum = underTest.sumNumbers(root);
-
-        System.out.println(sum);
-
+        assertEquals(1026, sum);
     }
 
+    @Test
+    void testSingleNode() {
+        var root = new TreeNode(4);
+        int sum = underTest.sumNumbers(root);
+        assertEquals(4, sum);
+    }
+
+    @Test
+    void testOneLevelDepthTree() {
+        var root = new TreeNode(4);
+        root.addLeft(1);
+        root.addRight(0);
+        int sum = underTest.sumNumbers(root);
+        assertEquals(81, sum);
+    }
+
+    @Test
+    void testLeftBranchOnlyTree() {
+        var root = new TreeNode(4);
+        root
+            .addLeft(1)
+            .addLeft(1)
+            .addLeft(1);
+        int sum = underTest.sumNumbers(root);
+        assertEquals(4111, sum);
+    }
+
+    @Test
+    void testNonZeroLeafTree() {
+        var root = new TreeNode(0);
+        root
+            .addLeft(0)
+            .addRight(1);
+        int sum = underTest.sumNumbers(root);
+        assertEquals(1, sum);
+    }
 
 }
